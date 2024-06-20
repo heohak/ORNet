b054524df1effe5a046d6632a10bc4a30d7765bf
Samuel <saarge@ttu.ee>
2024-06-20 12:23:53 +0300

    springboot initialization

---
+++ src/main/java/com/demo/bait/BaitApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/bait/BaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
