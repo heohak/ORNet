34dddf1223b8c0218754b1fce0b0cb46eedc0ea6
qidoqga <leplandjoosep28@gmail.com>
2024-07-04 11:11:07 +0300

    bait partneri töötaja entity lisatud

---
+++ src/main/java/com/demo/bait/entity/BaitWorker.java
@@ -0,0 +1,25 @@
+package com.demo.bait.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class BaitWorker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String title;
+}
