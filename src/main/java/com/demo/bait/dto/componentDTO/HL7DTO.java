7d4bb023ac245f95180b3a3db2cb06db6ab45ceb
qidoqga <leplandjoosep28@gmail.com>
2024-07-10 12:14:34 +0300

    localDateTime muudetud LocalTime-iks

--- src/main/java/com/demo/bait/dto/componentDTO/HL7DTO.java
+++ src/main/java/com/demo/bait/dto/componentDTO/HL7DTO.java
@@ -2,8 +2,9 @@ package com.demo.bait.dto.componentDTO;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Builder
-public record HL7DTO(String vendorName, String version, LocalDateTime updateDate) {
+public record HL7DTO(String vendorName, String version, LocalDate updateDate) {
 }
