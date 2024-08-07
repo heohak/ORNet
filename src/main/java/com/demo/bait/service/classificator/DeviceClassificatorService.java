97dbaf534bd9a67ff3038aa54808001bee069718
qidoqga <leplandjoosep28@gmail.com>
2024-08-07 14:19:12 +0300

    device classificator update endpoint

--- src/main/java/com/demo/bait/service/classificator/DeviceClassificatorService.java
+++ src/main/java/com/demo/bait/service/classificator/DeviceClassificatorService.java
@@ -5,11 +5,14 @@ import com.demo.bait.dto.classificator.DeviceClassificatorDTO;
 import com.demo.bait.entity.classificator.DeviceClassificator;
 import com.demo.bait.mapper.classificator.DeviceClassificatorMapper;
 import com.demo.bait.repository.classificator.DeviceClassificatorRepo;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -19,11 +22,27 @@ public class DeviceClassificatorService {
     private DeviceClassificatorRepo deviceClassificatorRepo;
     private DeviceClassificatorMapper deviceClassificatorMapper;
 
+    @Transactional
     public ResponseDTO addDeviceClassificator(DeviceClassificatorDTO deviceClassificatorDTO) {
         DeviceClassificator deviceClassificator = new DeviceClassificator();
         deviceClassificator.setName(deviceClassificatorDTO.name());
         deviceClassificatorRepo.save(deviceClassificator);
         return new ResponseDTO("Device classificator added successfully");
+    }
+
+    @Transactional
+    public ResponseDTO updateDeviceClassificator(Integer deviceClassificatorId,
+                                                 DeviceClassificatorDTO deviceClassificatorDTO) {
+        Optional<DeviceClassificator> deviceClassificatorOpt = deviceClassificatorRepo.findById(deviceClassificatorId);
+        if (deviceClassificatorOpt.isEmpty()) {
+            throw new EntityNotFoundException("Device classificator with id " + deviceClassificatorId + " not found");
+        }
+        DeviceClassificator classificator = deviceClassificatorOpt.get();
+        if (deviceClassificatorDTO.name() != null) {
+            classificator.setName(deviceClassificatorDTO.name());
+        }
+        deviceClassificatorRepo.save(classificator);
+        return new ResponseDTO("Device classificator updated successfully");
     }
 
     public List<DeviceClassificatorDTO> getAllClassificators() {
