10027f3d955b65c9b324a39e274cfe072a1773b3
qidoqga <leplandjoosep28@gmail.com>
2024-09-06 15:47:34 +0300

    location telefoni numbri pattern

--- src/main/java/com/demo/bait/controller/LocationController/LocationPutController.java
+++ src/main/java/com/demo/bait/controller/LocationController/LocationPutController.java
@@ -3,6 +3,7 @@ package com.demo.bait.controller.LocationController;
 import com.demo.bait.dto.LocationDTO;
 import com.demo.bait.dto.ResponseDTO;
 import com.demo.bait.service.LocationServices.LocationService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,7 @@ public class LocationPutController {
     public final LocationService locationService;
 
     @PutMapping("/update/{locationId}")
-    public ResponseDTO updateLocation(@PathVariable Integer locationId, @RequestBody LocationDTO locationDTO) {
+    public ResponseDTO updateLocation(@PathVariable Integer locationId,@Valid @RequestBody LocationDTO locationDTO) {
         return locationService.updateLocation(locationId, locationDTO);
     }
 }
--- src/main/java/com/demo/bait/dto/LocationDTO.java
+++ src/main/java/com/demo/bait/dto/LocationDTO.java
@@ -1,7 +1,17 @@
 package com.demo.bait.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
-public record LocationDTO(Integer id, String name, String address, String phone) {
+public record LocationDTO(
+        Integer id,
+        @NotBlank(message = "Name cannot be empty")
+        String name,
+        @NotBlank(message = "Address cannot be empty")
+        String address,
+        @Pattern(regexp = "^\\+?[0-9 ]{1,15}$", message = "Invalid phone number format")
+        String phone
+) {
 }
