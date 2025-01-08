14c8f1871f6e89d3d31f6b4160e781732ba9509b
qidoqga <leplandjoosep28@gmail.com>
2025-01-08 16:01:22 +0200

    added new necessary fields to third party IT and endpoints to use them

--- src/main/java/com/demo/bait/mapper/ThirdPartyITMapper.java
+++ src/main/java/com/demo/bait/mapper/ThirdPartyITMapper.java
@@ -1,17 +1,29 @@
 package com.demo.bait.mapper;
 
 import com.demo.bait.dto.ThirdPartyITDTO;
+import com.demo.bait.entity.FileUpload;
 import com.demo.bait.entity.ThirdPartyIT;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ThirdPartyITMapper {
 
     List<ThirdPartyITDTO> toDtoList(List<ThirdPartyIT> thirdPartyITList);
+    @Mapping(source = "contact.id", target = "contactId")
+    @Mapping(target = "fileIds", expression = "java(mapFilesToIds(thirdPartyIT.getFiles()))")
     ThirdPartyITDTO toDto(ThirdPartyIT thirdPartyIT);
+
+    default List<Integer> mapFilesToIds(Set<FileUpload> files) {
+        return files.stream()
+                .map(FileUpload::getId)
+                .collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/com/demo/bait/service/ThirdPartyITServices/ThirdPartyITFileUploadService.java
@@ -0,0 +1,68 @@
+package com.demo.bait.service.ThirdPartyITServices;
+
+import com.demo.bait.dto.FileUploadDTO;
+import com.demo.bait.dto.ResponseDTO;
+import com.demo.bait.entity.FileUpload;
+import com.demo.bait.entity.ThirdPartyIT;
+import com.demo.bait.entity.Ticket;
+import com.demo.bait.mapper.FileUploadMapper;
+import com.demo.bait.repository.ThirdPartyITRepo;
+import com.demo.bait.service.FileUploadServices.FileUploadService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ThirdPartyITFileUploadService {
+
+    private ThirdPartyITRepo thirdPartyITRepo;
+    private FileUploadService fileUploadService;
+    private FileUploadMapper fileUploadMapper;
+
+    @Transactional
+    public ResponseDTO uploadFilesToThirdPartyIT(Integer thirdPartyId, List<MultipartFile> files) throws IOException {
+        log.info("Starting file upload to third party IT with ID: {}", thirdPartyId);
+
+        Optional<ThirdPartyIT> thirdPartyITOpt = thirdPartyITRepo.findById(thirdPartyId);
+        if (thirdPartyITOpt.isEmpty()) {
+            log.error("Third Party IT with ID: {} not found", thirdPartyId);
+            throw new EntityNotFoundException("Third Party IT with id " + thirdPartyId + " not found");
+        }
+        ThirdPartyIT thirdPartyIT = thirdPartyITOpt.get();
+
+        log.debug("Uploading {} files to third party IT with ID: {}", files.size(), thirdPartyId);
+        Set<FileUpload> uploadedFiles = fileUploadService.uploadFiles(files);
+
+        thirdPartyIT.getFiles().addAll(uploadedFiles);
+        thirdPartyITRepo.save(thirdPartyIT);
+
+        log.info("Files uploaded successfully to third party IT with ID: {}. Total files uploaded: {}", thirdPartyIT,
+                uploadedFiles.size());
+        return new ResponseDTO("Files uploaded successfully to third party IT");
+    }
+
+    public List<FileUploadDTO> getThirdPartyITFiles(Integer thirdPartyId) {
+        log.info("Fetching files for third party IT with ID: {}", thirdPartyId);
+
+        Optional<ThirdPartyIT> thirdPartyITOpt = thirdPartyITRepo.findById(thirdPartyId);
+        if (thirdPartyITOpt.isEmpty()) {
+            log.error("Third Party IT with ID: {} not found", thirdPartyId);
+            throw new EntityNotFoundException("Third Party IT with id " + thirdPartyId + " not found");
+        }
+        ThirdPartyIT thirdPartyIT = thirdPartyITOpt.get();
+
+        List<FileUploadDTO> files = fileUploadMapper.toDtoList(thirdPartyIT.getFiles().stream().toList());
+        log.info("Found {} files for third party IT with ID: {}", files.size(), thirdPartyId);
+        return files;
+    }
+}
