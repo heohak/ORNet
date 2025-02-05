d795ba31dc3f27906ec6e7c54f3be465c433bdab
qidoqga <leplandjoosep28@gmail.com>
2025-02-05 11:51:44 +0200

    Maintenance entity changes added some fields and modified POST and PUT endpoints for maintenance update and add functions

--- src/main/java/com/demo/bait/service/DeviceServices/DeviceService.java
+++ src/main/java/com/demo/bait/service/DeviceServices/DeviceService.java
@@ -523,24 +523,4 @@ public class DeviceService {
             throw e;
         }
     }
-
-    public Set<Device> deviceIdsToDevicesSet(List<Integer> deviceIds) {
-        log.info("Converting device IDs to Device set: {}", deviceIds);
-        try {
-            Set<Device> devices = new HashSet<>();
-            for (Integer deviceId : deviceIds) {
-                Device device = deviceRepo.findById(deviceId)
-                        .orElseThrow(() -> {
-                            log.warn("Invalid device ID: {}", deviceId);
-                            return new IllegalArgumentException("Invalid device ID: " + deviceId);
-                        });
-                devices.add(device);
-            }
-            log.info("Converted {} device IDs to Device set.", devices.size());
-            return devices;
-        } catch (Exception e) {
-            log.error("Error while converting device IDs to Device set: {}", deviceIds, e);
-            throw e;
-        }
-    }
 }
